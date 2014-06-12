e8c5bcee1d9b2e75ba0df45f527f9f28118b3086
yauheni selivonchyk <y.selivonchyk@gmail.com>
2014-06-12 10:13:22 +0300

    InitialCommit

---
+++ TabChecker.UI/TabChecker.BLL/Logic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TabChecker.BLL
+{
+    public static class Logic
+    {
+	    public static string GetInfo()
+	    {
+		    return "TabChecker";
+	    }
+    }
+}
---
+++ TabChecker.UI/TabChecker.UI/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TabChecker.BLL;
+
+namespace TabChecker.UI
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(Logic.GetInfo());
+			Console.ReadLine();
+		}
+	}
+}
